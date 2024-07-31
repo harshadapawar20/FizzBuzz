2003a5bed0991a12e543aae61e1962608e3429a1
harshadapawar20 <117945017+harshadapawar20@users.noreply.github.com>
2024-07-31 13:18:17 +0530

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+n= 1
+
+while n<=100:
+    if n%3==0 and n%5==0:
+        print(n," -FizzBuzz")
+        
+    
+    elif n%5==0:
+        print(n,' -Buzz')
+         
+
+    elif n%3==0:
+        print(n,' -Fizz')
+       
+    
+    
+    
+    n=n+1
